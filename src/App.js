361110b74ab7604e6437eb594f975a6219c3d523
Rahul Goyal <rahulgoyal8312@gmail.com>
2021-10-04 23:05:25 +0530

    Subheader component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Products from "./components/Products/Products";
 import Header from "./components/Layout/Header"
+import Subheader from "./components/Layout/Subheader";
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Subheader/>
       <Products/>
     </div>
   );
