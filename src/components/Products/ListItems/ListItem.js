ed46d7fe2e185f0c8e5eee0cde53ce9a49f3153f
Rahul Goyal <rahulgoyal8312@gmail.com>
2021-10-14 00:07:43 +0530

    List rendering & key prop

--- src/components/Products/ListItems/ListItem.js
+++ src/components/Products/ListItems/ListItem.js
@@ -1,6 +1,10 @@
 import AddToCartIcon from "../../../assets/icons/add_cart.svg"
 
 const ListItem = ({ data }) => {
+    const handleClick = () => {
+        // Handle Button Click
+    }
+
     return (
         <div className={"item-card"}>
             <img className={"img-fluid"} src={`/assets/${data.thumbnail}`} alt={data.title}/>
@@ -12,10 +16,10 @@ const ListItem = ({ data }) => {
                     </small>
                 </div>
                 <div className={"title"}>
-                    <h3>{data.title}</h3>
+                    <h3>{title}</h3>
                 </div>
             </div>
-            <button className={"cart-add"}>
+            <button className={"cart-add"} onClick={handleClick}>
                 <span>Add to Cart</span>
                 <img src={AddToCartIcon} alt="Cart Icon"/>
             </button>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,28 +1,49 @@
+import { useState } from "react"
 import ListItem from "./ListItems/ListItem"
 
-const items = [
-    {
-        id: 0,
-        title: "Title of this Item 1",
-        price: 450,
-        discountedPrice: 340,
-        thumbnail: "placeholder.png"
-    },
-    {
-        id: 1,
-        title: "Title of this Item 2",
-        price: 100,
-        discountedPrice: 80,
-        thumbnail: "placeholder.png"
-    }
-]
-
 const Products = () => {
+    const [items, setItems] = useState([
+        {
+            id: 0,
+            title: "Title of this Item 1",
+            price: 450,
+            discountedPrice: 340,
+            thumbnail: "placeholder.png"
+        },
+        {
+            id: 1,
+            title: "Title of this Item 2",
+            price: 100,
+            discountedPrice: 80,
+            thumbnail: "placeholder.png"
+        },
+        {
+            id: 2,
+            title: "Title of this Item 3",
+            price: 100,
+            discountedPrice: 80,
+            thumbnail: "placeholder.png"
+        },
+        {
+            id: 3,
+            title: "Title of this Item 4",
+            price: 100,
+            discountedPrice: 80,
+            thumbnail: "placeholder.png"
+        }
+    ])
+
     return (
         <div className={"product-list"}>
             <div className={"product-list--wrapper"}>
-                <ListItem data={items[0]}></ListItem>
-                <ListItem data={items[1]}></ListItem>
+                {/* <ListItem data={items[0]}></ListItem>
+                <ListItem data={items[1]}></ListItem> */}
+                {
+                    items.map(item => {
+                        return (<ListItem key={item.id} data={item}/>)
+                    })
+                }
+                {/* {[<ListItem data={item[0]}/>,<ListItem data={item[1]}/>,<ListItem data={item[3]}/>]} */}
             </div>
         </div>
     )
