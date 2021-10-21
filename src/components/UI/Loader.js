a0cced278e127bd4f2d895a5d06997fdbbd7851a
Rahul Goyal <rahulgoyal8312@gmail.com>
2021-10-22 00:59:40 +0530

    Project-dynamic-Modal

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -1,10 +1,22 @@
 import ReactDom from 'react-dom'
 
+export const Backdrop = props => {
+    const handleClick = () => {
+        if(props.onClose) {
+            props.onClose();
+        }
+    }
+
+    return (
+        <div onClick={handleClick} className="loader-overlay"></div>
+    )
+}
+
 const Loader = () => {
     return (
         ReactDom.createPortal(
             <>
-            <div className="loader-overlay"></div>
+            <Backdrop/>
             <div className="loading-dots">
                 <div>Loading</div>
                 <div className="loading-dots--dot"></div>
