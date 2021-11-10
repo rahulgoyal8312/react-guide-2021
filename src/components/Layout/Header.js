0d7065a2ab5769008263258509cdf24c0f42ea72
Rahul Goyal <rahulgoyal8312@gmail.com>
2021-11-11 00:11:00 +0530

    Minor link fix

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom"
 import { logout } from "../../actions/auth"
 import Cart from "../Cart"
 import SearchBox from "../UI/Search"
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
     const history = useHistory()
@@ -16,7 +17,7 @@ const Header = () => {
     return (
         <header>
             <div className="nav-brand">
-                <a to="/">
+                <NavLink to={"/"}>
                     <span>AmaKart</span>
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-shopping-cart" width="30"
                         height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round"
@@ -27,7 +28,7 @@ const Header = () => {
                         <path d="M17 17h-11v-14h-2" />
                         <path d="M6 5l14 1l-1 7h-13" />
                     </svg>
-                </a>
+                </NavLink>
             </div>
             <div className="searchBox-container">
                 <SearchBox/>
